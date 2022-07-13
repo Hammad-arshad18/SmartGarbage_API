cc9297a0ae317d4a77a7551708f407e66f9099ba
Hammad-Arshad <18101001-072@uskt.edu.pk>
2022-07-13 20:40:47 +0500

    Admin Panel Theme Change

--- SmartGarbage_API/urls.py
+++ SmartGarbage_API/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+admin.site.site_header="Smart Garbage"
+admin.site.site_title="Admin SGM"
+admin.site.index_title = "Home"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('api.urls')),
